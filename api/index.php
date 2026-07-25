a70e2a774c1e12abb726720178bfe93521fd2826
aditprogaming <m.aditiya1805@gmail.com>
2026-07-25 11:43:20 +0700

    Fix Vercel serverless storage directory

--- api/index.php
+++ api/index.php
@@ -1,4 +1,22 @@
 <?php
 
-// Forward request to Laravel public/index.php
+// Buat folder temporary untuk Vercel serverless
+$tmpDirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($tmpDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
+// Set environment variable untuk cache path
+putenv('APP_STORAGE_RESPONSE_PATH=/tmp/storage');
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+
 require __DIR__ . '/../public/index.php';
